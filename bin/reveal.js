e9656c23e7c4009700a7954218eb7a4ef9d0b65d
Welling Guzman <hola@wellingguzman.com>
2019-01-21 22:34:27 -0400

    allow pipe on bin

--- bin/reveal.js
+++ bin/reveal.js
@@ -3,11 +3,47 @@
 const reveal = require('..');
 const args = process.argv.slice(2);
 
-if (args.length == 0) {
-    console.error('Usage: reveal.js <url>');
+function revealUrl(url) {
+  if (!url) {
+    console.error('Missing URL');
     process.exit(-1);
+  }
+
+  reveal(url)
+    .then(console.log)
+    .catch(console.error);
 }
 
-reveal(args[0])
-    .then(console.log)
-    .catch(err => console.log(err.message));
+function getUrl() {
+  return new Promise(function (resolve, reject) {
+    if (args.length >= 1) {
+      resolve(args[0]);
+    } else {
+      getUrlFromStdIn().then(resolve).catch(reject);
+    }
+  });
+}
+
+function getUrlFromStdIn() {
+  return new Promise(function (resolve, reject) {
+    var data = '';
+
+    process.stdin.setEncoding('utf8');
+
+    process.stdin.on('data', function (chunk) {
+      data += chunk;
+    });
+
+    process.stdin.on('end', function () {
+      resolve(data);
+    });
+
+    process.stdin.on('error', reject);
+
+    process.openStdin();
+  });
+}
+
+getUrl()
+  .then(revealUrl)
+  .catch(console.error);
